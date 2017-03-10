117aa81bbf2cd0d3218f1f79943712bbad958fd0
fsilva02 <filipe.a.silva01@gmail.com>
2017-03-10 15:00:43 +0000

    hi

--- player_fsilva/src/fsilva_node.cpp
+++ player_fsilva/src/fsilva_node.cpp
@@ -6,9 +6,12 @@
 //test include rws message
 #include <rwsua2017_msgs/MakeAPlay.h>
 
+// ROS include
+#include<ros/ros.h>
 //indent your code!
 
 using namespace std;
+using namespace boost;
 
 namespace rwsua2017
 {
@@ -16,28 +19,41 @@ namespace rwsua2017
   class MyPlayer: public Player
   {
     public:
+     ros::Subscriber sub;
 
     MyPlayer(string argin_name, string argin_team_name): Player(argin_name, argin_team_name)
     {
-      cout << "Initialized MyPlayer" << endl;
+
+      sub = n.subscribe("/make_a_play", 1000, &MyPlayer::makeAPlayCallback, this);
+      cout << "Initialized MyPlayer\n" << endl;
     };
 
     vector<string> teammates;
 
-  };
+
+
+  void makeAPlayCallback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg)
+  {
+    cout << "Message received";
+  }
+   };
 }
 
 
-int main()
+int main(int argc, char **argv)
 {
-  cout << "Hello world" << endl;
+ // cout << "Hello world" << endl;
+    ros::init(argc, argv, "player_fsilva");
+
+    ;
 
   rwsua2017::MyPlayer myplayer("player_fsilva", "green");
 
   cout << "name = " << myplayer.name << endl;
   cout << "team name = " << myplayer.get_team_name() << endl;
 
-  myplayer.teammates.push_back("vsilva");
+  myplayer.teammates.push_back("jferreira");
+  myplayer.teammates.push_back("jferreira");
 
   //ciclo for tipico em c
   //int i;
@@ -49,6 +65,6 @@ int main()
     cout << myplayer.teammates[i] << endl;
   }
 
-
+    ros::spin();
   return 1;
 }
